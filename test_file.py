5d479e4c5ae0ce8108c5d45deff4dc35b5912f7d
melloru <pythonubuntu12@gmail.com>
2024-05-11 17:45:47 +0300

    change tests - assert

--- test_file.py
+++ test_file.py
@@ -1,12 +1,8 @@
 from file import capitalize
 
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
 
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
-
+assert capitalize('') == ''
 
 print('Все тесты пройдены!')
